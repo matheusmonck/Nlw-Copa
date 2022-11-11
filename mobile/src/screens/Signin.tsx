0cf9d4f488ec4efa33fcbf1038c9eb32e614df74
Kalley Matheus <matheusmonck@gmail.com>
2022-11-11 16:39:22 -0300

    mobile aula 1

---
+++ mobile/src/screens/Signin.tsx
@@ -0,0 +1,11 @@
+import { Center, Text } from 'native-base';
+
+export function Signin() {
+  return (
+    <Center flex={1} bgColor='gray.900'>
+      <Text fontSize={24} color='white'>
+        Signin
+      </Text>
+    </Center>
+  );
+}
